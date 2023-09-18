0c5e2ef879698d36c790e32364a4fdb95c50b71c
Brady Freeman <onebrady@gmail.com>
2023-09-18 09:27:54 -0500

    change image sizes

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ function Home() {
       width={2560}
       height={1440}
       className='root-bg'
+      sizes='(max-width:768px) 100vw, (max-width:1919px) 1920px'
       />
       <App />
       </>
